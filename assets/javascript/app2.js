b4d92bb24ccd8e92c71454a5c27f139d2eb2fff1
davebattles <davidoflores@gmail.com>
2018-05-07 12:45:45 -0500

    index2.html app2.js with leaflet api alternative api testing

---
+++ assets/javascript/app2.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+
+  var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoiZGF2aWRvZmxvcmVzIiwiYSI6ImNqZ3NjdmVvbTAxaHcyenF0cWViOXA1cWsifQ.oVUanCyMScIkw_DKQWxGpQ'
+}).addTo(mymap);
+
+
+
+  $.getJSON("resturants.json", function (data) {
+    var citycoordx = data.location.latitude;
+    var citycoordy = data.location.longitude;
+
+    mymap.flyTo(new L.LatLng(citycoordx, citycoordy));
+  
+  
+  
+
+    console.log(data);
+
+    console.log(data.nearby_restaurants[0]);
+    console.log(typeof data.nearby_restaurants);
+
+
+
+    for (i = 0; i < data.nearby_restaurants.length; i++) {
+      // set vars to x and y for coord use
+      var xcoord = data.nearby_restaurants[i].restaurant.location.latitude;
+      var ycoord = data.nearby_restaurants[i].restaurant.location.longitude;
+      var marker = L.marker([xcoord, ycoord]).addTo(mymap)
+      ;
+      marker.bindPopup(data.nearby_restaurants[i].restaurant).openPopup();
+    }
+
+
+
+    console.log(data.location.latitude);
+
+
+  });
+
+
+});
